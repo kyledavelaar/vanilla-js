4813c9068e1d22e261bf20b478aa74a9f6ebd0c9
Kyle Davelaar <kyle@Kyles-MacBook-Pro.local>
2018-07-22 15:58:18 -0700

    add building blocks

---
+++ animation/building-blocks/animation.js
@@ -0,0 +1,19 @@
+// const colors = ['red', 'green', 'blue', 'pink', 'orange', 'yellow'];
+
+// const container = document.getElementById('container');
+
+// for (let i = 0; i < 6; i += 1) {
+//   let div = document.createElement('div');
+//   container.appendChild(div);
+//   div.setAttribute('style',
+//     `
+//     background: ${colors[i]}; 
+//     height: 50px; width: 50px;
+//     animation: 
+//     `
+//   )
+
+// }
+
+// animation: drop1 3s ease 0s;
+// animation-fill-mode: forwards;
